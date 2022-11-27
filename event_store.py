7c2773be33a31050c29c1296acd07558fe600b7b
Nathan Boukobza <boukobza.nathan@gmail.com>
2022-11-27 18:19:59 +0100

    add update and trying to fix delete

--- event_store.py
+++ event_store.py
@@ -12,19 +12,21 @@ class DatetimeEventStore:
     def get_events(self, start, end):
         events_to_get = []
         for event in self.events:
-            if self.validate(start) <= event["date"] <= self.validate(end):
+            if self.validate(start) <= self.validate(event["date"]) <= self.validate(end):
                 events_to_get.append(event)
         return events_to_get
 
+    def update_event(self, id, data):
+        self.events[id] = {"date": self.validate(data['date']), "event": data['event']}
+
     def delete_event(self, events_to_remove):
         for event in events_to_remove:
-            for i in range(len(self.events) - 1):
+            for i in range(len(self.events)):
                 if self.events[i] == event:
                     self.events.pop(i)
 
     @staticmethod
     def validate(date_to_validate):
-        print(date_to_validate)
         if not isinstance(date_to_validate, date):
             try:
                 date_to_validate = datetime.strptime(date_to_validate, '%Y-%m-%d %H:%M:%S')
