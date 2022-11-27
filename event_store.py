062a1a12e7fce525a33d0aee7bbce6d8b1fde734
Nathan Boukobza <boukobza.nathan@gmail.com>
2022-11-27 12:00:04 +0100

    initial commit

---
+++ event_store.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+
+
+class DatetimeEventStore:
+    def __init__(self):
+        pass
+
+    def store_event(self, at, event):
+        pass
+
+    def get_events(self, start, end):
+        pass
